2ab2237f3e30d5106d29c908308a9a01a8b2ffed
IWGDevelop <desarrollador1@iwglogistics.com>
2018-08-10 14:51:50 -0500

    Getter V1

---
+++ Scripts/Common/getters.js
@@ -0,0 +1,236 @@
+var textComplete = "1";
+var onlyCode = "Code";
+var onlyDescription = "Description";
+var onlyName = "Name";
+function getJsonForDropDrown(url, dropDown, textType, extraOption) {
+    var _this = this;
+    on();
+    $.getJSON(url)
+        .done(function (data) {
+        dropDown.removeAttr("disabled");
+        dropDown.empty();
+        dropDown.append('<option value="">Seleccione</option>');
+        $.each(data, function (key, val) {
+            switch (textType) {
+                case textComplete:
+                    if (extraOption === "") {
+                        dropDown.append('<option value="' + val.Id + '">' + val.Code + " - " + val.Description + '</option>');
+                    }
+                    else {
+                        dropDown.append('<option value="' + val.Id + '" data-' + extraOption + '=' + val[extraOption] + '>' + val.Code + " - " + val.Description + '</option>');
+                    }
+                    break;
+                case onlyCode:
+                    if (extraOption === "") {
+                        dropDown.append('<option value="' + val.Id + '">' + val.Code + '</option>');
+                    }
+                    else {
+                        dropDown.append('<option value="' + val.Id + '" data-' + extraOption + '=' + val[extraOption] + '>' + val.Code + '</option>');
+                    }
+                    break;
+                case onlyDescription:
+                    if (extraOption === "") {
+                        dropDown.append('<option value="' + val.Id + '" data-cod="' + val.Code + '">' + val.Description + '</option>');
+                    }
+                    else {
+                        dropDown.append('<option value="' + val.Id + '" data-cod="' + val.Code + '" data-' + extraOption + '=' + val[extraOption] + '>' + val.Description + '</option>');
+                    }
+                    break;
+                default:
+                    if (extraOption === "") {
+                        dropDown.append('<option value="' + val.Id + '" data-cod="' + val.Code + '">' + val[textType] + '</option>');
+                    }
+                    else {
+                        dropDown.append('<option value="' + val.Id + '" data-cod="' + val.Code + '" data-' + extraOption + '=' + val[extraOption] + '>' + val[textType] + '</option>');
+                    }
+                    break;
+            }
+        });
+        off();
+    })
+        .fail(function (jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error + ", " + jqxhr.responseJSON != null ? '' : jqxhr.responseJSON.MessageDetail;
+        _this.objCommon.showNotification("ERROR", "Respuesta fallida <br> Servicio: " + url + "<br>" + err, "Web API");
+        off();
+    });
+}
+function getJsonForBootstrapSelect(url, options) {
+    var _this = this;
+    var dropDowns = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        dropDowns[_i - 2] = arguments[_i];
+    }
+    options = setDefaults(options, defaultsBootstrapSelect);
+    on();
+    var extraText = "";
+    $.getJSON(url)
+        .done(function (data) {
+        var _loop_1 = function (dropDown) {
+            if (options.enable) {
+                dropDown.removeAttr("disabled");
+            }
+            dropDown.empty();
+            $.each(data, function (key, val) {
+                extraText = "";
+                if (options.extraOption != "" && options.extraOption != undefined && options.extraOption != null) {
+                    extraText += ' data-' + options.extraOption + '=' + val[options.extraOption];
+                }
+                if (options.subTextOption != "" && options.subTextOption != undefined && options.subTextOption != null) {
+                    var opts = options.subTextOption.split('.');
+                    var obj = val;
+                    for (var i = 0; i < opts.length; i++) {
+                        obj = obj[opts[i]];
+                    }
+                    if (options.limitSubTextOption > 0) {
+                        if (obj.length < options.limitSubTextOption) {
+                            extraText += ' data-subtext="' + obj + '"';
+                        }
+                        else {
+                            extraText += ' data-subtext="' + obj.substr(0, options.limitSubTextOption) + '..."';
+                        }
+                    }
+                    else {
+                        extraText += ' data-subtext="' + obj + '"';
+                    }
+                }
+                dropDown.append('<option value="' + val.Id + '"' + extraText + '>' + val[options.text] + '</option>');
+            });
+            dropDown.selectpicker('refresh');
+            off();
+        };
+        for (var _i = 0, dropDowns_1 = dropDowns; _i < dropDowns_1.length; _i++) {
+            var dropDown = dropDowns_1[_i];
+            _loop_1(dropDown);
+        }
+    })
+        .fail(function (jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error + ", " + jqxhr.responseJSON != null ? '' : jqxhr.responseJSON.MessageDetail;
+        _this.objCommon.showNotification("ERROR", "Respuesta fallida <br> Servicio: " + url + "<br>" + err, "Web API");
+        off();
+    });
+}
+function getJsonForMultiselect(url) {
+    var _this = this;
+    var multiselect = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        multiselect[_i - 1] = arguments[_i];
+    }
+    on();
+    $.getJSON(url)
+        .done(function (data) {
+        var listData = [];
+        $.each(data, function (key, val) {
+            listData.push(val);
+        });
+        for (var i = 0; i < multiselect.length; i++) {
+            multiselect[i].data("kendoMultiSelect").setDataSource(new kendo.data.DataSource({ data: listData }));
+        }
+        off();
+    })
+        .fail(function (jqxhr, textStatus, error) {
+        var err = textStatus + ", " + error + ", " + jqxhr.responseJSON != null ? '' : jqxhr.responseJSON.MessageDetail;
+        _this.objCommon.showNotification("ERROR", "Respuesta fallida <br> Servicio: " + url + "<br>" + err, "Web API");
+        off();
+    });
+}
+function getProperty(url, editor, property, errorMessage) {
+    on();
+    $.getJSON(url)
+        .done(function (data) {
+        if (data != null) {
+            editor.val(data[property]);
+        }
+        off();
+    })
+        .fail(function (jqxhr, textStatus, error) {
+        if (errorMessage == undefined || errorMessage == null) {
+            swal("Error", "Ha ocurrido un error al hacer la solicitud.\n\n" + error, "error");
+        }
+        else {
+            swal("Error", "Ha ocurrido un error al hacer la solicitud.\n\n" + errorMessage, "error");
+        }
+        off();
+    });
+}
+function getCode(url, editor) {
+    getProperty(url, editor, "Code");
+}
+function getDescription(url, editor) {
+    getProperty(url, editor, "Description");
+}
+function getObject(url) {
+    var object;
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            if (data != null) {
+                if (data.length == 1) {
+                    object = data[0];
+                }
+                else {
+                    object = data;
+                }
+            }
+            else {
+                object = null;
+            }
+        }
+    });
+    return object;
+}
+function getCurrentDate() {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var tDay = day.toString();
+    var tMonth = month.toString();
+    if (day < 10) {
+        tDay = '0' + day;
+    }
+    if (month < 10) {
+        tMonth = '0' + month;
+    }
+    return year + "-" + tMonth + "-" + tDay;
+}
+//Función para generar un consecutivo usando el servicio publicado de generación de consecutivos para un documento específico
+function getConsecutive(field, idDocumentId, businessUnitId, serviceTypeId, companyId, saveOp) {
+    on();
+    $.ajax({
+        url: apiTransverse + "DocumentTypes/GetConsecutive",
+        dataType: 'json',
+        data: {
+            idDocument: idDocumentId,
+            businessUnit: businessUnitId,
+            serviceType: serviceTypeId,
+            company: companyId,
+            save: saveOp
+        },
+        success: (function (data) {
+            field.val(data);
+            off();
+        }),
+        error: (function (jqXHR, textStatus, error) {
+            swal("Error", "No hemos logrado conseguir un consecutivo para este documento.\n\nPor favor, inténtalo de nuevo en un momento y si el problema persiste contacta con el administrador", "error");
+            off();
+        })
+    });
+}
+//Classes
+var OptionsBootstrapSelect = /** @class */ (function () {
+    function OptionsBootstrapSelect() {
+    }
+    return OptionsBootstrapSelect;
+}());
+var defaultsBootstrapSelect = {
+    text: "Description",
+    extraOption: "",
+    enable: false,
+    subTextOption: "",
+    limitSubTextOption: 0
+};
+function setDefaults(options, defaults) {
+    return Object.assign({}, defaults, options);
+}
